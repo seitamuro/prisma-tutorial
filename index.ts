6d25d39147ae699246c96e5e486b79b782adc86e
seitamuro <seitamuro0210@gmail.com>
2023-09-24 22:31:57 +0900

    ユーザーを作成するスクリプトを作成

--- index.ts
+++ index.ts
@@ -3,8 +3,26 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient
 
 async function main() {
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+  await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      posts: {
+        create: { title: 'Hello World' }
+      },
+      profile: {
+        create: { bio: 'I like turtles' },
+      }
+    }
+  })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    }
+  })
+  console.dir(allUsers)
 }
 
 main()
